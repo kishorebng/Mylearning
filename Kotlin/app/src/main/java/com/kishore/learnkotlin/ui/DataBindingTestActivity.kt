ebf7d38d8ef0111625127acc0112675d43e697af
kkumar1 <k.kumar1@partner.samsung.com>
2019-02-08 17:46:03 -0500

    Kotlin Learning

--- Kotlin/app/src/main/java/com/kishore/learnkotlin/ui/DataBindingTestActivity.kt
+++ Kotlin/app/src/main/java/com/kishore/learnkotlin/ui/DataBindingTestActivity.kt
@@ -8,27 +8,25 @@ import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProviders
 import com.kishore.learnkotlin.R
-import com.kishore.learnkotlin.databinding.DatbinActivityBinding
-import com.kishore.learnkotlin.databinding.LiveTeamScore
-import com.kishore.learnkotlin.databinding.TeamScore
-import com.kishore.learnkotlin.databinding.TeamScoreViewModel
-import com.kishore.learnkotlin.viewmodel.livedata.LiveScoreViewModel
+import com.kishore.learnkotlin.databinding.*
 import java.util.*
 
 class DataBindingTestActivity : AppCompatActivity() {
 
     private lateinit var binding: DatbinActivityBinding
 
-    private lateinit var teamScore: TeamScore
+    private lateinit var teamAScore: TeamScore
     private lateinit var mViewModel: TeamScoreViewModel
     private lateinit var mLiveViewModel: LiveTeamScore
+    private lateinit var teamDScore: TeamScoreObservable
+
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this,
                 R.layout.datbin_activity)
-        teamScore = TeamScore()
+        teamAScore = TeamScore()
         setTeamAbinding()
 
         mViewModel = ViewModelProviders.of(this).get(TeamScoreViewModel::class.java)
@@ -41,13 +39,16 @@ class DataBindingTestActivity : AppCompatActivity() {
                 setTeamCbinding()
             }
         })
-        setTeamCbinding();
+        setTeamCbinding()
+
+        teamDScore = TeamScoreObservable()
+        setTeamDbinding()
     }
 
     fun addOneForTeamA(v: View) {
         val rnd = Random()
-        teamScore = TeamScore()
-        teamScore.score = rnd.nextInt(50)
+        teamAScore = TeamScore()
+        teamAScore.score = rnd.nextInt(50)
     }
 
     fun updateTeamA(v: View) {
@@ -67,8 +68,27 @@ class DataBindingTestActivity : AppCompatActivity() {
         mLiveViewModel.teamScore.value = rnd.nextInt(10);
     }
 
+
+    fun addOneForTeamD(v: View) {
+        val rnd = Random()
+        teamDScore = TeamScoreObservable()
+        teamDScore.observableScore = rnd.nextInt(50) + 2
+    }
+
+
+    fun addOneForTeamE(v: View) {
+    }
+
+    fun updateTeamE(v: View) {
+        setTeamAbinding()
+    }
+
+    fun addOneForTeamF(v: View) {
+    }
+
+
     fun setTeamAbinding() {
-        binding.setTeamAScore(teamScore)
+        binding.setTeamAScore(teamAScore)
     }
 
     fun setTeamBbinding() {
@@ -79,11 +99,16 @@ class DataBindingTestActivity : AppCompatActivity() {
         binding.setTeamCScore(mLiveViewModel)
     }
 
+    fun setTeamDbinding() {
+        binding.setTeamDScore(teamDScore)
+    }
+
+
     /**
      * Resets the score for all teams back to 0.
      */
     fun resetScore(v: View) {
-        teamScore = TeamScore()
+        teamAScore = TeamScore()
         setTeamAbinding()
         mViewModel.score = 0;
         setTeamBbinding()
